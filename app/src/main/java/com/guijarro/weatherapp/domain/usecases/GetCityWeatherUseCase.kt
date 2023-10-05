257b0eff025f1d430e6d3b6a27a8fc3e5cf6718b
gguij004 <51983923+gguij004@users.noreply.github.com>
2023-10-04 22:16:20 -0400

    part 2
    Added Domain Layer (UseCases) and Domain mapper

---
+++ app/src/main/java/com/guijarro/weatherapp/domain/usecases/GetCityWeatherUseCase.kt
@@ -0,0 +1,18 @@
+package com.guijarro.weatherapp.domain.usecases
+
+import com.guijarro.weatherapp.data.rest.WeatherRepository
+import com.guijarro.weatherapp.domain.WeatherDomain
+import com.guijarro.weatherapp.utils.UIState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetCityWeatherUseCase @Inject constructor(
+    private val weatherRepository: WeatherRepository
+) {
+    operator fun invoke(city: String): Flow<UIState<WeatherDomain>> = flow {
+        weatherRepository.getCityWeather(city).collect {
+            emit(it)
+        }
+    }
+}
